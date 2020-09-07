6be2a43d86ec22bac94c81dbd292d2f3ffde299a
e0426051 <e0426051@u.nus.edu>
2020-09-07 08:27:16 -0700

    REDO Lecture activity

---
+++ src/main/java/animal/Bird.java
@@ -0,0 +1,7 @@
+package circus.animal;//import Circus.circus.animal.Animal;
+
+public abstract class Bird extends Animal {
+    public void fly() {
+        System.out.println("Whee ...");
+    }
+}
